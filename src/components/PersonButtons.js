f5d8159caf34bd0b69cfc68bc86ce8be19ae466e
tahircdincer <149729519+tahircdincer@users.noreply.github.com>
2024-08-28 20:03:42 +0300

    ' alt ' attribute was added into ' PersonButtons.js '

--- src/components/PersonButtons.js
+++ src/components/PersonButtons.js
@@ -5,11 +5,11 @@ export default function PersonButtons() {
   return (
     <div className="btn-container">
       <button className="btn btn__email">
-        <img src={IconImage} />
+        <img src={IconImage} alt= " Email Icon " />
         <span>Email</span>
       </button>
       <button className="btn btn__linkedin">
-        <img src={LinkedInImage} />
+        <img src={LinkedInImage} alt= " LinkedIn Icon " />
         <span>LinkedIn</span>
       </button>
     </div>
